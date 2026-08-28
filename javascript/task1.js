6463db79facf023271e15bf0f771332351fca033
HrithikDasoju <dasojuhrithikkumar@thestackly.com>
2026-08-28 11:52:42 +0530

    Add files via upload

---
+++ javascript/task1.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+var a = 500
+a=1000
+console.log(a);
+var a=30000
+console.log(a);
+let age = "john"
+age = "david"
+console.log(age);
+confirm()
+document.writeln(age)
