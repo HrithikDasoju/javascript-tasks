8a94fa5afa59510bd9fe684b660b4b29549e8768
HrithikDasoju <dasojuhrithikkumar@thestackly.com>
2026-09-02 17:21:21 +0530

    Add files via upload

---
+++ javascript/task2.js
@@ -0,0 +1,190 @@
+// var studentname = "Kiran" ;
+// let studentAge = 21 ;
+// const collegeName = "Au";
+// console.log(studentname);
+// console.log(studentAge);
+// console.log(collegeName);
+// studentname = "Rahul";
+// studentAge=20;
+//  collegeName="JNTU";
+// var studentname ="Manish"
+// console.log(studentname);
+//  let studentAge=18;
+
+// var name1;
+// var age;
+// let city;
+
+// name1 = prompt("enter your name");
+// console.log(name1);
+// age = prompt("enter age");
+// console.log(age);
+// city=prompt("enter city name");
+// console.log(city);
+
+
+// alert("Welcome")
+// let n=prompt("enter name")
+// alert(n+" welcome")
+
+//   ! Age Calculator
+
+// let year = prompt("enter birthYear")
+// console.log(2026-year);
+
+// ^
+// var a="Hello"
+// var b=100
+// var c=25.5
+// var d=true
+// var e=false
+// var f=undefined
+// var g=null
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(f));
+
+// & studentData
+
+let student = {
+    name: "mahesh",
+    age:21,
+    city:"Hyd",
+    qualification:"B.Tech",
+}
+console.log(student.name);
+console.log(student.age);
+console.log(student.city);
+console.log(student.qualification);
+
+//  ^ Fruit array
+
+let fruit = ["Apple","Mango","Orange","Banana","Grapes","Papaya"]
+console.log(fruit[0])
+console.log(fruit[1]);
+let fr=fruit.length-1
+console.log(fruit[fr]);
+console.log(fruit.length)
+
+// * Arithematic operators
+
+// ^ calculator
+
+let ab=20
+let bc=5
+console.log(ab+bc);
+console.log(ab-bc);
+console.log(ab*bc);
+console.log(ab/bc);
+console.log(ab%bc);
+console.log(ab^bc);
+
+// ^ Shopping bill
+let shirt = 999
+let pant = 1499
+let shoes = 1999
+let total = shirt+pant+shoes
+console.log("Total Shopping bill "+ total);
+
+// ^ Marks
+
+let tamil=80
+let english=75
+let maths=90
+const totalMarks=tamil+english+maths
+console.log(totalMarks);
+const avgMarks=totalMarks/3;
+console.log(avgMarks);
+
+// ^
+// let a = 10;
+
+// let b = a++;
+
+// console.log(a); // 11
+// console.log(b); // 10
+// Write your expected output first, then run it.
+// Task 12 — Pre Increment
+// Predict the output:
+// let a = 10;
+
+// let b = ++a;
+
+// console.log(a); // 11
+// console.log(b); //11
+// Task 13 — Post Decrement
+// Predict:
+// let a = 20;
+
+// let b = a--;
+
+// console.log(a); // 19
+// console.log(b); // 20
+// Task 14 — Pre Decrement
+// Predict:
+// let a = 20;
+
+// let b = --a;
+
+// console.log(a); // 19
+// console.log(b); // 19
+
+let a = 5;
+
+let b = a++;
+
+let c = ++a;
+
+let d = b--;
+
+console.log(a); // 7
+console.log(b); // 4
+console.log(c); // 7
+console.log(d); // 5
+
+
+// *
+let num =10
+num +=5
+console.log(num);
+num-=5
+console.log(num);
+num*=5
+console.log(num);
+num/=5
+console.log(num);
+num%=5
+console.log(num);
+num**=5
+console.log(num);
+
+// ! student object
+let st={
+    name:"kiran",
+    age:19,
+    college:"OU",
+    subjects:["english","telugu","hindi","tamil","maths"],
+    marks:[80,82,87,89,90],
+    city:"hyd"
+}
+ 
+let x =prompt("enter x value")
+let y = prompt("enter y value")
+
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x*y)
+console.log(x**y)
+console.log(x+y)
+
+
+
+
+
+
+
+
